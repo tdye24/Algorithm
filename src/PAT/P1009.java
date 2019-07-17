5f124f830e5cc73275c5c0d75e7a42ed4ae36dd3
TDYe123 <18365225454@163.com>
2019-07-17 23:31:39 +0800

    PAT109.PAT1027

---
+++ src/PAT/P1009.java
@@ -0,0 +1,50 @@
+package PAT;
+
+import java.util.*;
+import java.io.*;
+public class P1009 {
+	public static double[] f = new double[1024];
+	public static double[] g = new double[2048];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedInputStream(System.in));
+		int ex = 0;
+		double co = 0;
+		
+		int Anum = sc.nextInt();
+		for(int i=0; i<Anum; i++) {
+			ex = sc.nextInt();
+			co = sc.nextDouble();
+			f[ex] += co;
+		}
+		
+		int Bnum = sc.nextInt();
+		for(int i=0; i<Bnum; i++) {
+			ex = sc.nextInt();
+			co = sc.nextDouble();
+			for(int j=0; j<1024; j++) {
+				if(f[j] != 0) {
+					g[ex+j] += co*f[j];
+				}
+			}
+			
+		}
+		
+		int K = 0;
+		for(int i=0; i<2048; i++) {
+			if(g[i] != 0) {
+				K++;
+			} 
+		}
+		System.out.print(K);
+		
+		for(int i=2047; i>=0; i--) {
+			if(g[i] != 0) {
+				System.out.printf(" %d %.1f", i, g[i]);
+			}
+			
+		}
+
+	}
+
+}
---
+++ src/PAT/P1027.java
@@ -0,0 +1,22 @@
+package PAT;
+
+import java.util.*;
+import java.io.*;
+public class P1027 {
+	public static Character[] table = new Character[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C'};
+	public static String solve(int num) {
+		String str = "";
+		str += table[num/13];
+		str += table[num%13];
+		return str;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedInputStream(System.in));
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		System.out.println("#" + solve(num1) + solve(num2) + solve(num3));
+	}
+
+}
