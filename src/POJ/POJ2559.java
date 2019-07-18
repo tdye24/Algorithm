dac32fe6ce6778718120031fa45c2d525f2511e9
TDYe123 <18365225454@163.com>
2019-07-18 23:00:07 +0800

    fix:int->BigInteger

--- src/POJ/POJ2559.java
+++ src/POJ/POJ2559.java
@@ -2,12 +2,13 @@ package POJ;
 
 import java.util.*;
 import java.io.*;
+import java.math.BigInteger;
 
 class Item {
-	int height;
-	int width;
+	BigInteger height;
+	BigInteger width;
 	
-	public Item(int height, int width) {
+	public Item(BigInteger height, BigInteger width) {
 		this.height = height;
 		this.width = width;
 	}
@@ -18,35 +19,35 @@ public class POJ2559 {
 		Scanner sc = new Scanner(new BufferedInputStream(System.in));
 		Stack<Item> stack = new Stack<>();
 		while(true) {
-			int temp = 0;
-			int max = 0;
+			BigInteger temp = new BigInteger("0");
+			BigInteger max = new BigInteger("0");
 			int n = sc.nextInt();
 			if(n == 0) {
 				break ;
 			}
 			for(int i=0; i<n; i++) {
-				Item item = new Item(sc.nextInt(), 1);
-				if(stack.isEmpty() || item.height > stack.peek().height) {
+				Item item = new Item(sc.nextBigInteger(), new BigInteger("1"));
+				if(stack.isEmpty() || item.height.compareTo(stack.peek().height) > 0) {
 					stack.push(item);
 				} else {
 					Item deItem = null;
-					while(item.height <= stack.peek().height) {
+					while(item.height.compareTo(stack.peek().height) <= 0) {
 						deItem = stack.pop();
-						temp = deItem.height*deItem.width;
-						if(max < temp) {
+						temp = deItem.height.multiply(deItem.width);
+						if(max.compareTo(temp) < 0) {
 							max = temp;
 						}
-						if(stack.isEmpty() || item.height > stack.peek().height) 
+						if(stack.isEmpty() || item.height.compareTo(stack.peek().height) > 0) 
 							break ;
-						stack.peek().width += deItem.width;
+						stack.peek().width = stack.peek().width.add(deItem.width);
 					}
-					item.width += deItem.width;
+					item.width = item.width.add(deItem.width);
 					stack.push(item);
 				}
 			}
 			while(!stack.isEmpty()) {
-				temp = stack.peek().height*stack.pop().width;
-				if(max < temp) {
+				temp = stack.peek().height.multiply(stack.pop().width);
+				if(max.compareTo(temp) < 0) {
 					max = temp;
 				}
 			}
