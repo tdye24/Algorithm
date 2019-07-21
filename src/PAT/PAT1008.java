3b4eda21963fe8503935f3f8855a8aa735b24b2c
TDYe123 <18365225454@163.com>
2019-07-21 22:40:32 +0800

    PAT1008

---
+++ src/PAT/PAT1008.java
@@ -0,0 +1,28 @@
+package PAT;
+
+import java.util.*;
+import java.io.*;
+public class PAT1008 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedInputStream(System.in));
+		int N = sc.nextInt();
+		int previous = 0;
+		int current = 0;
+		int height = 0;
+		int sum = 0;
+		for(int i=0; i<N; i++) {
+			current = sc.nextInt();
+			height = current - previous;
+			if(height >= 0) {
+				sum += 6*height;
+			} else {
+				sum -= 4*height;
+			}
+			previous = current;;
+		}
+		sum += 5*N;
+		System.out.println(sum);
+	}
+
+}
