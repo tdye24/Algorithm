5f04236bdf9ff5cabaa0c52326ecd36405244e33
TDYe123 <18365225454@163.com>
2019-07-23 13:59:41 +0800

    HDU5616 TimeExceedLimit

--- src/HUD/HDU5616.java
+++ src/HUD/HDU5616.java
@@ -7,27 +7,33 @@ public class HDU5616 {
 	static final int MAX_N = 24;
 	static final int MAX_W = 24*100;
 	static int[] w = new int[MAX_N];
-	static int[][] dp = new int[MAX_N][2*MAX_W+1];//0表示false, 1表示true, 初始化为-1, 表示前i个砝码正反选是否能组成重量为j的砝码组
-
-	static int rec(int i, int j) {
+	static boolean[][] dp = new boolean[MAX_N][2*MAX_W+1];//0表示false, 1表示true, 初始化为-1, 表示前i个砝码正反选是否能组成重量为j的砝码组
+	static void ini() {
+		for(int i=0; i<MAX_N; i++) {
+			for(int j=0; j<MAX_W; j++) {
+				dp[i][j] = false;
+			}
+		}
+	}
+	static boolean rec(int i, int j) {
 		if(j > 2*sum || j <= 0) 
-			return 0;
-		if(dp[i][j] > 0) {
+			return false;
+		if(dp[i][j]) {
 			//已经计算过直接使用以前结果
 			return dp[i][j];
 		}
-		int res = 0 ;
+		boolean res = false;
 		if(j == sum) {
-			res = 1;
+			res = true;
 		} else if(i == 0) {
-			res = (w[i] == (2*sum -  j))||(w[i] == j) ? 1:0;
+			res = (w[i] == (2*sum -  j))||(w[i] == j) ? true:false;
 		} else {
-			if(rec(i-1, j-w[i]+sum) == 1) 
-				return dp[i][j] = 1;
-			if(rec(i-1, j+w[i]-sum) == 1) 
-				return dp[i][j] = 1;
-			if(rec(i-1, j) == 1) {
-				return dp[i][j] = 1;
+			if(rec(i-1, j-w[i]+sum)) 
+				return dp[i][j] = true;
+			if(rec(i-1, j+w[i]-sum)) 
+				return dp[i][j] = true;
+			if(rec(i-1, j)) {
+				return dp[i][j] = true;
 			}
 		}
 		return dp[i][j] = res;
@@ -35,8 +41,9 @@ public class HDU5616 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(new BufferedInputStream(System.in));
 		int T = sc.nextInt();//测试样例
-		int N = sc.nextInt();//砝码个数
 		for(int i=0; i<T; i++) {
+			ini();
+			int N = sc.nextInt();//砝码个数
 			for(int j=0; j<N; j++) {
 				w[j] = sc.nextInt();
 				sum += w[j];
@@ -46,7 +53,7 @@ public class HDU5616 {
 			}
 			int M = sc.nextInt();//查询次数
 			for(int k=0; k<M; k++) {
-				System.out.println(rec(N-1, sc.nextInt()+sum) == 1 ? "YES":"NO");
+				System.out.println(rec(N-1, sc.nextInt()+sum) ? "YES":"NO");
 			}
 		}
 	}
