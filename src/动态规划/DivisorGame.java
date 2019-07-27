07a747d71abb39d0f3520211de3ca698d8bb1465
TDYe123 <18365225454@163.com>
2019-07-27 13:25:28 +0800

    DivisorGame

--- src/动态规划/DivisorGame.java
+++ src/动态规划/DivisorGame.java
@@ -7,11 +7,12 @@ public class DivisorGame {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(new BufferedInputStream(System.in));
 		int n = sc.nextInt();
+		sc.close();
 		if(n%2 == 0) 
 			System.out.print("true");
 		else 
 			System.out.print("false");
-
+		
 	}
 
 }
