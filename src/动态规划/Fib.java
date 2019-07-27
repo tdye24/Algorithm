4f8c22b0fd71d5ae19d06b7991a99b3e1355329b
TDYe123 <18365225454@163.com>
2019-07-27 11:41:16 +0800

    Fib.DP

---
+++ src/动态规划/Fib.java
@@ -0,0 +1,18 @@
+package ¶¯Ì¬¹æ»®;
+
+import java.util.*;
+import java.io.*;
+public class Fib {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedInputStream(System.in));
+		int n = sc.nextInt();
+		int[] dp = new int[1024];
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i=2; i<=n; i++) 
+			dp[i] = dp[i-1]+dp[i-2];
+		System.out.print(dp[n]);
+	}
+
+}
