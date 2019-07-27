66a19869d873831477248fe80fdc42044707da56
TDYe123 <18365225454@163.com>
2019-07-27 13:23:32 +0800

    DivisorGame

---
+++ src/动态规划/DivisorGame.java
@@ -0,0 +1,17 @@
+package ¶¯Ì¬¹æ»®;
+
+import java.util.*;
+import java.io.*;
+public class DivisorGame {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new BufferedInputStream(System.in));
+		int n = sc.nextInt();
+		if(n%2 == 0) 
+			System.out.print("true");
+		else 
+			System.out.print("false");
+
+	}
+
+}
