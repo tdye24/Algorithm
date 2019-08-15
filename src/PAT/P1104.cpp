7ab81944de1846a3988c39bdcf7e995837fcf26d
TDYe123 <18365225454@163.com>
2019-08-15 12:19:23 +0800

    PAT1104.找规律.巨坑（data*i*(N+1-i)不能写成i*(N+1-i)*data->越界）

--- src/PAT/P1104.cpp
+++ src/PAT/P1104.cpp
@@ -1,21 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 int N;
 int main() {
-	float data;
-	float ans = 0;
-	int cnt = 0;
+	double data;
+	double ans = 0.0;
 	scanf("%d", &N);
 	for(int i=1; i<=N; i++) { //第几个数，输入时即处理 
-		scanf("%f", &data);
-		for(int j=1; j<=N; j++) { //区间长度 
-			if(i>=j) 
-				cnt = min(N-i+1, j);
-			else 
-				cnt = min(N-j+1, i);
-			ans += data*cnt;
-		}
+		scanf("%lf", &data);
+		ans += data*i*(N+1-i); //ans += i*(N+1-i)*data;最后两个测试点错误，原因i*(N+1-i)越界 
 	} 
-	printf("%.2f\n", ans);
+	printf("%.2lf\n", ans);
 	return 0;
 }
