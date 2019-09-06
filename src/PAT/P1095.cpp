c668ab0cccf75bde4409cb8d1f51182dace3c76d
TDYe123 <18365225454@163.com>
2019-09-06 23:20:26 +0800

    PAT1095.map的使用->自动按照字典序排列

--- src/PAT/P1095.cpp
+++ src/PAT/P1095.cpp
@@ -1,44 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn = 1010;
-struct Node {
-	bool flag;
-	int data, left, right, level;
-} nodes[maxn];
-int N, data, suma, sumb, maxl;
+struct node {
+	char id[10];
+	int time, flag = 0; //flag = 1->进入;flag = -1->离开
+};
+
+bool cmp1(node a, node b) {
+	if(strcmp(a.id, b.id) != 0)
+		return strcmp(a.id, b.id) < 0;
+	else 
+		return a.time < b.time;
+}
+
+bool cmp2(node a, node b) {
+	return a.time < b.time;
+}
+
 int main() {
-	cin >> N;
-	for(int i=0; i<N; i++) {
-		cin >> data;
-		int root = 0, level = 0;
-		while(nodes[root].flag) {
-			if(data <= nodes[root].data) {
-				if(nodes[root].left == 0) 
-					nodes[root].left = i;
-				root = nodes[root].left;
-			} else {
-				if(nodes[root].right == 0) 
-					nodes[root].right = i;
-				root = nodes[root].right;
-			}	
-			level++;
-		}
-		nodes[root].flag = true;
-		nodes[root].data = data;
-		nodes[root].level = level;
-		maxl = max(maxl, level);
+	int n, k, maxtime, tempindex = 0;
+	scanf("%d%d\n", &n, &k);
+	vector<node> record(n), car;
+	for(int i=0; i<n; i++) {
+		char temp[5];
+		int h, m, s;
+		scanf("%s %d:%d:%d %s\n", record[i].id, &h, &m, &s, temp);
+		int temptime = h*3600 + m*60 + s;
+		record[i].time = temptime;
+		record[i].flag = strcmp(temp, "in") == 0 ? 1 : -1;
 	}
-	for(int i=0; i<N; i++) {
-		if(nodes[i].level == maxl) {
-			suma++;
-			continue;
-		}	
-		if(nodes[i].level == maxl-1) {
-			sumb++;
-			continue;
+	sort(record.begin(), record.end(), cmp1);
+	map<string, int> mp;
+	for(int i=0; i<n-1; i++) {
+		if(strcmp(record[i].id, record[i+1].id) == 0 && record[i].flag == 1 && record[i+1].flag == -1) {
+			car.push_back(record[i]);
+			car.push_back(record[i+1]);
+			mp[record[i].id] += (record[i+1].time - record[i].time);
+			if(maxtime < mp[record[i].id])
+				maxtime = mp[record[i].id];
 		}
-			
 	}
-	cout << suma << " + " << sumb << " = " << suma+sumb << endl;
+	sort(car.begin(), car.end(), cmp2);
+	vector<int> cnt(n);
+	for(int i=0; i<car.size(); i++) {
+		if(i == 0)
+			cnt[i] += car[i].flag; 
+		else
+			cnt[i] = cnt[i-1] + car[i].flag; //这个flag有点好用
+	}
+	for(int i=0; i<k; i++) {
+		int h, m, s;
+		scanf("%d:%d:%d", &h, &m, &s);
+		int temptime = h*3600 + m*60 + s;
+		int j;
+		for(j = tempindex; j<car.size(); j++) {
+			if(car[j].time > temptime) {
+				printf("%d\n", cnt[j-1]);
+				break;
+			} else if(j == car.size()-1) {
+				printf("%d\n", cnt[j]); //查询时间靠最后
+			}
+			tempindex = j;
+		}
+	}
+	for(auto it:mp) { //map本来就按照字典序排列
+		if(it.second == maxtime)
+			printf("%s ", it.first.c_str());
+	}
+	printf("%02d:%02d:%02d", maxtime/3600, (maxtime%3600)/60, maxtime%60);
 	return 0;
 }
